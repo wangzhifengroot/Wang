2003b5c2dfad307a0800bd6f61be0d8e294134a4
wangzhifengroot <1441289824@qq.com>
2020-02-23 17:25:31 +0800

    修改App项目使它成为测试项目，修改支持Kotlin，来测试java和Kotlin的代码。删除无用的代码

--- app/src/main/java/com/example/wang/base/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.example.wang.base;
-
-/**
- * 创建人：v_wangzhifeng01
- * 创建时间：2019-05-24
- */
-public class Constant {
-
-}
--- app/src/main/java/com/example/wang/test/TestActivity.java
+++ app/src/main/java/com/example/wang/test/TestActivity.java
@@ -1,41 +1,20 @@
 package com.example.wang.test;
 
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.example.wang.R;
-import com.example.wang.base.BaseActivity;
-import com.example.wang.base.BasePresenter;
 
-public class TestActivity extends BaseActivity {
 
-    private TextView net;
-    private String TAG = "TestActivity";
+public class TestActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        initView();
-    }
 
-    @Override
-    public BasePresenter getPresenter() {
-        return null;
-    }
 
-    @Override
-    protected void initVariables() {
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    private void initView() {
-        net = (TextView) findViewById(R.id.net);
     }
 }
