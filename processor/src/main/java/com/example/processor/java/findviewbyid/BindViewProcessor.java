ef03f6a965e3b9d8c06b8ec173404501dc82137e
wangzhifengroot <1441289824@qq.com>
2021-03-24 19:33:27 +0800

    升级gradle并且跑通apt

--- processor/src/main/java/com/example/processor/java/findviewbyid/BindViewProcessor.java
+++ processor/src/main/java/com/example/processor/java/findviewbyid/BindViewProcessor.java
@@ -17,7 +17,6 @@ import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
